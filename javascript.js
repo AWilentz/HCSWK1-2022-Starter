a2dd0f030638eaeaf434a07ce02353da0f435972
Alex Wilentz <awilentz@college.harvard.edu>
2022-02-20 18:17:49 -0500

    created website

--- javascript.js
+++ javascript.js
@@ -1,1 +1,5 @@
 /* input JavaScript code here! */
+function show() {
+    document.getElementById("no_display").style.display = "block";
+    document.getElementById("button").style.display = "none";
+}
