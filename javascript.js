1ff624fc53f27e4ef381e72347e1aca8bc7d77a8
Alex Wilentz <awilentz@college.harvard.edu>
2022-02-22 12:35:39 -0500

    updates

--- javascript.js
+++ javascript.js
@@ -2,4 +2,10 @@
 function display() {
     document.getElementById("no_display").style.display = "block";
     document.getElementById("button").style.display = "none";
+    setTimeout( function() { hide(); }, 3000);
+}
+
+function hide() {
+    document.getElementById("no_display").style.display = "none";
+    document.getElementById("button").style.display = "block";
 }
